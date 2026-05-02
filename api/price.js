e118fa527c4df90613bfc92cabe829dded4aa70f
aruizcar10-droid <aruizcar10@gmail.com>
2026-05-02 18:26:12 +0100

    Update price.js

--- api/price.js
+++ api/price.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const r = await fetch(
-      'https://query1.finance.yahoo.com/v8/finance/chart/XAG=F?interval=1d&range=30d',
+      'https://query1.finance.yahoo.com/v8/finance/chart/XAGUSD=X?interval=1d&range=30d',
       { headers: { 'User-Agent': 'Mozilla/5.0' } }
     );
     const data = await r.json();
