30b39f29ea322e52d2d040ddf18e63f935ecb740
aruizcar10-droid <aruizcar10@gmail.com>
2026-05-02 18:27:34 +0100

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,13 +1,1 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try {
-    const r = await fetch(
-      'https://query1.finance.yahoo.com/v8/finance/chart/XAGUSD=X?interval=1d&range=30d',
-      { headers: { 'User-Agent': 'Mozilla/5.0' } }
-    );
-    const data = await r.json();
-    return res.status(200).json(data);
-  } catch(e) {
-    return res.status(500).json({ error: e.message });
-  }
-}
+'https://query1.finance.yahoo.com/v8/finance/chart/SI=F?interval=1d&range=30d',
